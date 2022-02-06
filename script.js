9d0839f0e8c23ea6cb4ce7d9423a438149b63ac4
Ramy Ahmed <ramy.g.ahmed@gmail.com>
2022-02-06 12:43:02 +0000

    Made and interactive UI for Users to play

--- script.js
+++ script.js
@@ -4,54 +4,52 @@ function computerPlay(){
 }
 let playerScore = 0;
 let computerScore = 0;
+let result = document.getElementById("Result");
+let score = document.getElementById("Score");
+let playerSelection = "Please select a choice from the above";
+const compSelection=document.getElementById("CompSelection");
+
+
+document.addEventListener('click', function(e){
+    if(e.target.tagName=="BUTTON"){
+        if (playerScore <= 4 && computerScore <= 4){
+        playerSelection = e.target.textContent
+        result.textContent = playRound();
+        }else{
+            if(playerScore > computerScore){result.textContent = "You won the game"}
+            else{result.textContent = "You lost the game"};
+        }
+    }
+})
+
 function playRound(){
-    computerSelection = computerPlay();
-    console.log (`Computer chose: ${computerSelection}`);
-    playerSelection = prompt("Choose Rock, Paper or Scissors").toLowerCase();
-    console.log (`You chose: ${playerSelection}`);
-    let winner = 'no game';
+    compSelection.textContent = computerPlay();
+    computerSelection = compSelection.textContent;
+    document.getElementById("playSelection").textContent = playerSelection;
+    let winner = '';
     switch (playerSelection) {
         case "rock":
             if(computerSelection == "rock"){winner='Tie'}
-            else if (computerSelection == "paper") {winner = 'You Lose'}
-            else {winner = 'You Win'}
+            else if (computerSelection == "paper") {winner = 'You Lose'; computerScore++}
+            else {winner = 'You Win'; playerScore++}
             break;
         case "paper":
-            if(computerSelection == "rock"){winner='You Win'}
+            if(computerSelection == "rock"){winner='You Win'; playerScore++}
             else if (computerSelection == "paper") {winner = 'Tie'}
-            else {winner = 'You Lose'}
+            else {winner = 'You Lose'; computerScore++}
             break;
         case "scissors":
-            if(computerSelection == "rock"){winner='You Lose'}
-            else if (computerSelection == "paper") {winner = 'You Win'}
+            if(computerSelection == "rock"){winner='You Lose'; computerScore++}
+            else if (computerSelection == "paper") {winner = 'You Win'; playerScore++}
             else {winner = 'Tie'}
             break;
 
         default:
             break;
     }
+    score.textContent = `You: ${playerScore} - Computer: ${computerScore}`;
     return winner;
+}
 
-}
-function game(){
-    for (let x = 0; x < 5; x++) {
-            let winner = playRound();
-            if (winner == "You Win") {
-                console.log('You Win!')
-                playerScore++;
-            } 
-            else if (winner == "You Lose"){
-                console.log('You Lose!')
-                computerScore++;
-            }else{
-                x--;
-                console.log('Tie');
-            }
-    }
-    if (playerScore > computerScore) {
-        console.log("You win the Game!")
-    } else if (computerScore > playerScore) {
-        console.log("You lose the Game!")
-    }
-}
-console.log(game());
+
+//console.log(game());
